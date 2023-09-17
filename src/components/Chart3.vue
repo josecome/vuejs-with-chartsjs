a37d32b22c505027c7982ba8fbbff785959e1187
Jose Jaime Come <josejaimecome@gmail.com>
2023-09-17 22:20:48 +0200

    Minor changes made

--- src/components/Chart3.vue
+++ src/components/Chart3.vue
@@ -1,6 +1,15 @@
 <template>
     <div class="container">
-      <Bar v-if="loaded" :data="chartData" />
+      <Bar v-if="loaded" :data="{
+        labels: ['TV', 'LapTop', 'Freezer', 'AC', 'Table', 'Chair'],
+        datasets: [
+          {
+            label: 'Products',
+            data: [12, 19, 3, 5, 6, 3],
+            backgroundColor: 'rgba(103, 140, 249 , 1)'
+          }
+        ]
+      }" />
     </div>
   </template>
   
@@ -28,7 +37,12 @@
       try {
         const d = await axios.get('http://127.0.0.1:3000/data')
         this.chartdata = d.data
-
+        const dd = d.data
+        console.log('=====================')
+        this.labels = dd.labels
+        this.label = dd.datasets[0].label
+        this.data = dd.datasets[0].data
+        this.bck = dd.datasets[0].backgroundColor
   
         this.loaded = true
       } catch (e) {
